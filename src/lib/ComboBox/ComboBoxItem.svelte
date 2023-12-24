45b90f2f41e48ad3e789b0a894292d59542bc617
ThanhHM2 <thanhhm2@vng.com.vn>
2023-12-24 22:20:08 +0700

    update all item

---
+++ src/lib/ComboBox/ComboBoxItem.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	// import { createEventForwarder } from "$lib/internal.txt";
+	// import { get_current_component } from "svelte/internal";
+
+	import { createEventDispatcher } from 'svelte';
+
+	export let selected = false;
+	export let disabled = false;
+
+	let className = '';
+	export { className as class };
+
+	const dispath = createEventDispatcher();
+</script>
+
+<li
+	{...$$restProps}
+	class="combo-box-item {className}"
+	class:disabled
+	class:selected
+	tabindex="0"
+>
+	<slot name="icon" />
+	<span>
+		<slot />
+	</span>
+</li>
+
+<style lang="scss">
+  @use "./ComboBoxItem";
+</style>
